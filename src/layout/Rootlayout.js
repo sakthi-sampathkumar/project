603c366c6b1505e8668ef764aa68dd37aff702c3
sakthi-sampathkumar <20cs217sakthi@gmail.com>
2023-12-13 19:23:13 +0530

    new one

--- src/layout/Rootlayout.js
+++ src/layout/Rootlayout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "./firstnav";
 
  function Rootlayout(){
     return(
        <div>
-         <Outlet/>
+      <Navbar/>
+      <Outlet />
       </div>
     );
 }
