fdaa6fd02ed89c5295ef512f8ed45dbcaa3c42ce
sakthi-sampathkumar <20cs217sakthi@gmail.com>
2023-12-19 13:57:36 +0530

    aaaaaaaaa

--- src/firstdiv/home.jsx
+++ src/firstdiv/home.jsx
@@ -7,11 +7,11 @@ import Button from 'react-bootstrap/Button';
 
 export default function Home() {
   return (
-    <div className='background'>
+    <div className='background d-flex p-absolute'>
       <div className='textone'><h2><span style={{color:'gold'}}><IoHandLeftSharp size={20} /><IoHandRightSharp size={20}/></span>  Hello friends<br/>I Am Sofia And We Want To Start A <span  style={{color: 'violet'}}>Web Design</span> Course Together.<br/>Do You Like To Join It <span style={{color:'gold'}}><BsEmojiHeartEyesFill size={25}/></span> ?</h2>
-      <Button variant="dark mt-4">Start course now > </Button>
+      <Button variant="dark mt-4">Start course now > </Button><div className='discount'></div>
       </div>
-      
+      <div className='leftbackground ms-5 mt-4'></div>
     </div>
   )
 }
--- src/layout/secondnav.jsx
+++ src/layout/secondnav.jsx
@@ -14,16 +14,16 @@ function Secondnav() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
           <Nav.Link href="#link" className='computer ms-5'><MdOutlineComputer /></Nav.Link>
-            <Nav.Link href="#home">Python</Nav.Link>
-            <Nav.Link href="#link">React</Nav.Link>
-            <Nav.Link href="#link">Unity</Nav.Link>
+            <Nav.Link href="#home" className='py ms-2'>Python</Nav.Link>
+            <Nav.Link href="#link"className='py ms-4'>React</Nav.Link>
+            <Nav.Link href="#link"className='py ms-4'>Unity</Nav.Link>
             <Nav.Link href="#link" className='pen ms-5'> <FaPenNib /> </Nav.Link>
-            <Nav.Link href="#link">UI & UX</Nav.Link>
-            <Nav.Link href="#link">Vector</Nav.Link>
+            <Nav.Link href="#link"className='py ms-2'>UI & UX</Nav.Link>
+            <Nav.Link href="#link"className='py ms-4'>Vector</Nav.Link>
             <Nav.Link href="#link" className='camera ms-5'> <BsCameraReelsFill /> </Nav.Link>
-            <Nav.Link href="#link">After effects</Nav.Link>
-            <Nav.Link href="#link">Lightroom</Nav.Link>
-            <Nav.Link href="#link">Photography</Nav.Link>
+            <Nav.Link href="#link"className='py ms-2'>After effects</Nav.Link>
+            <Nav.Link href="#link"className='py ms-4'>Lightroom</Nav.Link>
+            <Nav.Link href="#link"className='py ms-4'>Photography</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
